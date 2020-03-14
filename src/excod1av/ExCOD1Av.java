605bea0fffdcdbb269cee809f9e4f80b1d9a96db
jaimgarod10 <jaimgarod@gmail.com>
2020-03-14 12:35:40 +0100

    F

--- src/excod1av/ExCOD1Av.java
+++ src/excod1av/ExCOD1Av.java
@@ -13,7 +13,7 @@ public class ExCOD1Av {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("D");
+        System.out.println("F");
     }
 
 }
